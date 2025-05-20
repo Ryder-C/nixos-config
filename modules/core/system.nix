82e20744a90ec2dab0b62847630319eb21502c46
Ryder-C <rydercasazza@gmail.com>
2025-05-19 17:56:38 -0700

    cachix for faster builds

--- modules/core/system.nix
+++ modules/core/system.nix
@@ -8,8 +8,13 @@
     settings = {
       auto-optimise-store = true;
       experimental-features = ["nix-command" "flakes"];
-      substituters = ["https://nix-gaming.cachix.org" "https://cuda-maintainers.cachix.org"];
-      trusted-public-keys = ["nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4=" "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="];
+      substituters = [
+        "https://nix-gaming.cachix.org"
+        "https://cuda-maintainers.cachix.org"
+        "https://cache.nixos.org/"
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = ["nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4=" "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E=" "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
     };
     gc = {
       automatic = false;
