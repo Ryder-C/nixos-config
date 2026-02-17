78be4fd6a3baff3e8cbd3eeba12404dcdffbc550
Ryder Casazza <rydercasazza@gmail.com>
2026-02-16 17:19:28 -0800

    jujutsu configuration

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -1,35 +1,58 @@
 {pkgs, ...}: {
-  programs.git = {
-    enable = true;
+  programs = {
+    jujutsu = {
+      enable = true;
 
-    settings = {
-      user = {
-        name = "Ryder-C";
-        email = "rydercasazza@gmail.com";
+      settings = {
+        user = {
+          name = "Ryder Casazza";
+          email = "rydercasazza@gmail.com";
+        };
+
+        ui = {
+          default-command = ["log" "-r" "main::"];
+          diff-formatter = ["difft" "--color=always" "$left" "$right"];
+        };
+
+        aliases = {
+          sync = ["rebase" "-s" "((roots((((::trunk()):: ~ ::trunk()) & mutable())..(::trunk()))-)+ & mutable()) ~ ::trunk()" "-o" "trunk()"];
+        };
       };
-      init.defaultBranch = "main";
-      credential.helper = "store";
     };
-  };
+    jjui.enable = true;
 
-  programs.lazygit = {
-    enable = true;
+    git = {
+      enable = true;
 
-    settings.gui = {
-      theme = {
-        activeBorderColor = ["#cba6f7" "bold"];
-        inactiveBorderColor = ["#a6adc8"];
-        optionsTextColor = ["#89b4fa"];
-        selectedLineBgColor = ["#313244"];
-        cherryPickedCommitBgColor = ["#45475a"];
-        cherryPickedCommitFgColor = ["#cba6f7"];
-        unstagedChangesColor = ["#f38ba8"];
-        defaultFgColor = ["#cdd6f4"];
-        searchingActiveBorderColor = ["#f9e2af"];
+      settings = {
+        user = {
+          name = "Ryder Casazza";
+          email = "rydercasazza@gmail.com";
+        };
+        init.defaultBranch = "main";
+        credential.helper = "store";
       };
+    };
 
-      authorColors = {
-        "*" = "#b4befe";
+    lazygit = {
+      enable = true;
+
+      settings.gui = {
+        theme = {
+          activeBorderColor = ["#cba6f7" "bold"];
+          inactiveBorderColor = ["#a6adc8"];
+          optionsTextColor = ["#89b4fa"];
+          selectedLineBgColor = ["#313244"];
+          cherryPickedCommitBgColor = ["#45475a"];
+          cherryPickedCommitFgColor = ["#cba6f7"];
+          unstagedChangesColor = ["#f38ba8"];
+          defaultFgColor = ["#cdd6f4"];
+          searchingActiveBorderColor = ["#f9e2af"];
+        };
+
+        authorColors = {
+          "*" = "#b4befe";
+        };
       };
     };
   };
