2b96453751e6039d152aa80e7d7c2be91e8ccfff
Ryder Casazza <rydercasazza@gmail.com>
2026-03-10 13:27:01 -0700

    switch to swayidle

--- modules/aspects/niri/_idle.nix
+++ modules/aspects/niri/_idle.nix
@@ -1,27 +1,32 @@
 {pkgs, ...}: {
-  services.hypridle = {
+  services.swayidle = {
     enable = true;
-    settings = {
-      general = {
-        after_sleep_cmd = "niri msg action power-on-monitors";
-        lock_cmd = "dms ipc lock lock";
-        before_sleep_cmd = "loginctl lock-session";
-      };
-
-      listener = [
-        # Lock screen after 5 min
-        {
-          timeout = 300;
-          "on-timeout" = "loginctl lock-session";
-        }
-
-        # Turn off monitors (DPMS) after 5.5 minutes
-        {
-          timeout = 330;
-          "on-timeout" = "niri msg action power-off-monitors; ${pkgs.openrgb-with-all-plugins}/bin/openrgb -p off";
-          "on-resume" = "niri msg action power-on-monitors; ${pkgs.openrgb-with-all-plugins}/bin/openrgb -p main";
-        }
-      ];
-    };
+    events = [
+      {
+        event = "before-sleep";
+        command = "loginctl lock-session";
+      }
+      {
+        event = "after-resume";
+        command = "niri msg action power-on-monitors";
+      }
+      {
+        event = "lock";
+        command = "dms ipc lock lock";
+      }
+    ];
+    timeouts = [
+      # Lock screen after 5 min
+      {
+        timeout = 300;
+        command = "loginctl lock-session";
+      }
+      # Turn off monitors (DPMS) after 5.5 minutes
+      {
+        timeout = 330;
+        command = "niri msg action power-off-monitors; ${pkgs.openrgb-with-all-plugins}/bin/openrgb -p off";
+        resumeCommand = "niri msg action power-on-monitors; ${pkgs.openrgb-with-all-plugins}/bin/openrgb -p main";
+      }
+    ];
   };
 }
