2d99c2714f52222e4284e6694c90850a0c92c5f7
Ryder Casazza <rydercasazza@gmail.com>
2026-05-10 21:45:10 -0700

    fix(praxis): aw watcher starting

--- modules/aspects/activitywatch.nix
+++ modules/aspects/activitywatch.nix
@@ -11,5 +11,17 @@
         };
       };
     };
+
+    systemd.user.services = {
+      activitywatch-watcher-awatcher = {
+        Unit.After = ["graphical-session.target"];
+        Service = {
+          Restart = "always";
+          RestartSec = "5s";
+          StartLimitBurst = 0;
+        };
+      };
+      activitywatch-watcher-aw-watcher-media-player.Service.Restart = "on-failure";
+    };
   };
 }
