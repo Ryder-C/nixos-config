c4ae77ed9b4f46fd208c2e767f4c175e744226c1
Ryder Casazza <rydercasazza@gmail.com>
2026-06-24 14:54:56 -0700

    add qui to media group

--- modules/aspects/nixflix/_qui.nix
+++ modules/aspects/nixflix/_qui.nix
@@ -4,4 +4,8 @@
     secretFile = config.age.secrets.qui.path;
     settings.host = "0.0.0.0";
   };
+
+  # qui needs to write the cross-seed hardlink tree under /storage/Torrents,
+  # which is group-writable only by `media`.
+  users.users.qui.extraGroups = ["media"];
 }
